52ca1d407d6791bfa1fb1e316ddd7f7974978f6d
Brian Haggard <brihagg@gmail.com>
2018-06-18 14:29:52 -0500

    Atom formatting - BH

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -2,25 +2,25 @@ import { expect } from 'chai';
 import { Item, GildedRose, production } from '../app/gilded-rose';
 
 describe('Run It', function() {
-    console.log('BEFORE');
-    production.items.forEach(item => {
-        console.log(JSON.stringify(item));
-    });
+  console.log('BEFORE');
+  production.items.forEach(item => {
+    console.log(JSON.stringify(item));
+  });
 
-    production.updateQuality();
+  production.updateQuality();
 
-    console.log('\n\nAFTER');
-    production.items.forEach(item => {
-        console.log(JSON.stringify(item));
-    });
+  console.log('\n\nAFTER');
+  production.items.forEach(item => {
+    console.log(JSON.stringify(item));
+  });
 });
 
 describe('Gilded Rose', function() {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+  it('should foo', function() {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('fixme');
+  });
 
 });
