972bedf409a1fe1fc3d9de2011c7368ecfb261a4
Brian Haggard <brihagg@gmail.com>
2018-10-26 13:07:34 -0500

    Fixed broken test

--- TypeScript/test/guilded-rose.spec.ts
+++ TypeScript/test/guilded-rose.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { Item, GildedRose, production } from '../app/gilded-rose';
+import { Item, TypedItem, GildedRose, production } from '../app/gilded-rose';
 
 describe('Run It', function() {
   console.log('BEFORE');
@@ -18,9 +18,9 @@ describe('Run It', function() {
 describe('Gilded Rose', function() {
 
   it('should foo', function() {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const gildedRose = new GildedRose([new TypedItem('foo', 0, 1, 'normal')]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    expect(items[0].quality).to.equal(0);
   });
 
 });
